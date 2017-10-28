eeabad9e63ae19e43ee86ea0551633f948156e01
Marcin Szoldrowski <marcin.szoldrowski@infakt.pl>
2017-10-29 00:54:03 +0200

    krs scraper

--- app/services/auctions/creator.rb
+++ app/services/auctions/creator.rb
@@ -12,9 +12,16 @@ module Auctions
         # klasa odpalająca pobranie z http://allegro.pl/sellerInfoFrontend/11303540/aboutSeller i zapisująca te dane do auctioneer_data
 
         hash_data = ::Scarpers::Auctioneer.perform(auction.auctioneer_id)
+        auction.auctioneer_data = Hash(auction.auctioneer_data).merge(hash_data)
+        auction.save
 
         # MOVE to DelayedJob
         # klasa odpalająca pobranie z http://allegro.pl/show_item.php?item=6993255084 i zapisująca te dane do auction_data
+
+        # MOVE to DelayedJob
+        # hash_data = ::Scarpers::Krs.perform(auction.auctioneer_data['company_nip'])
+        # auction.auctioneer_data = Hash(auction.auctioneer_data).merge(hash_data)
+        # auction.save
 
         # MOVE to DelayedJob
         ::Classifier::Client.instance.classify_auction(auction)
