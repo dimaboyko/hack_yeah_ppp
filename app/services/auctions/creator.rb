e8d1f9c07103339d5a80776b3cfc8e1cbac617d3
Marcin Szoldrowski <marcin.szoldrowski@infakt.pl>
2017-10-29 05:11:55 +0100

    stats

--- app/services/auctions/creator.rb
+++ app/services/auctions/creator.rb
@@ -8,24 +8,6 @@ module Auctions
 
     def perform
       ::Auction.create!(auction_data).tap { |auction|
-        # # MOVE to DelayedJob
-        # # klasa odpalająca pobranie z http://allegro.pl/sellerInfoFrontend/11303540/aboutSeller i zapisująca te dane do auctioneer_data
-
-        # auctioneer_data = ::Scarpers::Auctioneer.perform(auction.auctioneer_id)
-        # auction.auctioneer_data = Hash(auction.auctioneer_data).merge(auctioneer_data)
-        # auction.save
-
-        # # MOVE to DelayedJob
-        # # klasa odpalająca pobranie z http://allegro.pl/show_item.php?item=6993255084 i zapisująca te dane do auction_data
-
-        # auction_data = ::Scarpers::Auction.perform(auction.auction_id)
-
-        # # MOVE to DelayedJob
-        # krs_data = ::Scarpers::Krs.perform(auction.auctioneer_data['company_nip'])
-        # unless krs_data.empty?
-        #   auction.auctioneer_data = Hash(auction.auctioneer_data).merge(krs_data)
-        #   auction.save
-        # end
 
         # MOVE to DelayedJob
         ::Classifier::Client.instance.classify_auction(auction)
