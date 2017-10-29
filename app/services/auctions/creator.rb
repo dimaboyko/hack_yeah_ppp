eb32bdf75502543e9c80175cb8b404837fbd2ce6
Radek Rochmalski <radek@wredny.net>
2017-10-29 02:00:52 +0200

    add auction scraper

--- app/services/auctions/creator.rb
+++ app/services/auctions/creator.rb
@@ -11,12 +11,14 @@ module Auctions
         # MOVE to DelayedJob
         # klasa odpalająca pobranie z http://allegro.pl/sellerInfoFrontend/11303540/aboutSeller i zapisująca te dane do auctioneer_data
 
-        hash_data = ::Scarpers::Auctioneer.perform(auction.auctioneer_id)
-        auction.auctioneer_data = Hash(auction.auctioneer_data).merge(hash_data)
+        auctioneer_data = ::Scarpers::Auctioneer.perform(auction.auctioneer_id)
+        auction.auctioneer_data = Hash(auction.auctioneer_data).merge(auctioneer_data)
         auction.save
 
         # MOVE to DelayedJob
         # klasa odpalająca pobranie z http://allegro.pl/show_item.php?item=6993255084 i zapisująca te dane do auction_data
+
+        auction_data = ::Scarpers::Auction.perform(auction.auction_id)
 
         # MOVE to DelayedJob
         # hash_data = ::Scarpers::Krs.perform(auction.auctioneer_data['company_nip'])
