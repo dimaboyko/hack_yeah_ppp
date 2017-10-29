61b69a7597e2a3dd78baa8a2b5830ef9d9bdc39e
Adam Dybas <ad@xhtmllab.eu>
2017-10-29 04:19:42 +0100

    report toast

--- app/assets/javascripts/common/auction.js
+++ app/assets/javascripts/common/auction.js
@@ -1,61 +1,39 @@
+$(document).ready(function() {
 
-$(document).ready(function () {
-
-
-    var table = $('#table-data').DataTable({
-        language: {
-        search: "Wyszukaj w tabeli:",
-        lengthMenu: "Wyświetlaj _MENU_",
-        emptyTable: "Brak danych",
-        info: "Wyświetlone od _START_ do _END_ z _TOTAL_ rekordów",
-        infoEmpty: "Wyświetlone 0 rekordów",
-        infoFiltered: "(z _MAX_)",
-        zeroRecords: "Nie znaleziono rekordów spełniających kryteria wyszukiwania",
-        paginate: {
-          "first":      "First",
-          "last":       "Last",
-          "next":       "Następne",
-          "previous":   "Poprzednie"
-        },
+  var table = $('#table-data').DataTable({
+    language: {
+      search: "Wyszukaj w tabeli:",
+      lengthMenu: "Wyświetlaj _MENU_",
+      emptyTable: "Brak danych",
+      info: "Wyświetlone od _START_ do _END_ z _TOTAL_ rekordów",
+      infoEmpty: "Wyświetlone 0 rekordów",
+      infoFiltered: "(z _MAX_)",
+      zeroRecords: "Nie znaleziono rekordów spełniających kryteria wyszukiwania",
+      paginate: {
+        "first": "First",
+        "last": "Last",
+        "next": "Następne",
+        "previous": "Poprzednie"
       }
-    });
-
-    table.on( 'draw', function () {
-      $('.x-drawer-button').sideNav({
-          menuWidth: 488,
-          edge: 'right',
-          closeOnClick: false,
-          draggable: true
-        }
-      );
-    } );
-
-    $('select').material_select();
-
-  $('.x-propagate-stop').on('click', function(e){
-    e.stopPropagation();
+    }
   });
 
-  $('.x-drawer-button').sideNav({
-      menuWidth: 488,
-      edge: 'right',
-      closeOnClick: false,
-      draggable: true
-    }
-  );
+  table.on('draw', function() {
+    $('.x-drawer-button').sideNav({menuWidth: 488, edge: 'right', closeOnClick: false, draggable: true});
+  });
+
+  $('select').material_select();
+
+  $('.x-drawer-button').sideNav({menuWidth: 488, edge: 'right', closeOnClick: false, draggable: true});
 
   $('#modal').modal();
 
-  $('.select-dropdown').dropdown({
-     constrainWidth: false,
-     belowOrigin: true
-   }
- );
+  $('.select-dropdown').dropdown({constrainWidth: false, belowOrigin: true});
 
-  $('.dropdown-button').dropdown({
-     constrainWidth: false,
-     belowOrigin: true
-   }
- );
+  $('.dropdown-button').dropdown({constrainWidth: false, belowOrigin: true});
+
+  $('.x-msg').on('click', function(){
+    Materialize.toast('Wysłano zgłoszenie do kontroli.', 4000);
+  });
 
 });
